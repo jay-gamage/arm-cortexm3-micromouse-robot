ac7b9b7c673999cc94573b14205a73b95f47719a
jgamage94 <48186499+jgamage94@users.noreply.github.com>
2022-05-22 18:39:17 +0100

    Add files via upload

---
+++ pin_description.h
@@ -0,0 +1,32 @@
+#ifndef MBED_PIN_DESC
+#define MBED_PIN_DESC
+
+// MBED Pin Description:
+#define RIGHT_STEERING_PIN              p5
+#define LEFT_STEERING_PIN               p7
+
+#define MOTOR_LEFT_CLOCK_PIN            p14
+#define MOTOR_LEFT_DIR_PIN              p12
+#define MOTOR_RIGHT_CLOCK_PIN           p11
+#define MOTOR_RIGHT_DIR_PIN             p10
+
+#define LEFT_PROXIMITY_PIN              p9
+#define CENTER_PROXIMITY_PIN            p6
+#define RIGHT_PROXIMITY_PIN             p8 
+
+#define SET_SWITCH_PIN                  p27
+#define BEGIN_SWITCH_PIN                p28
+
+// PC1602F 16x2 Text LCD Pins:
+#define LCD_RS_PIN                      p21
+#define LCD_E_PIN                       p22
+#define LCD_D0_PIN                      p23
+#define LCD_D1_PIN                      p24
+#define LCD_D2_PIN                      p25
+#define LCD_D3_PIN                      p26
+
+// HC-SR04 Ultrasonic Sensor Pins:
+#define ULTRAOSNIC_TX_PIN               p29
+#define ULTRASONIC_RX_PIN               p30
+
+#endif
