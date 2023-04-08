abdbd48d57d29cfe4f9a9c53fcfd76c53d03e8c0
Jay Gamage <48186499+jay-gamage@users.noreply.github.com>
2023-04-08 13:01:00 +0100

    Add files via upload

--- pin_description.h
+++ pin_description.h
@@ -1,32 +1,34 @@
+// Header file contains pin description for MICROMOUSE - MATRIXBOT
+
 #ifndef MBED_PIN_DESC
 #define MBED_PIN_DESC
 
 // MBED Pin Description:
-#define RIGHT_STEERING_PIN              p5
-#define LEFT_STEERING_PIN               p7
+#define RIGHT_STEERING_PIN              p5          // Front panel - right steering sensor
+#define LEFT_STEERING_PIN               p7          // Front panel - left steering sensor
 
-#define MOTOR_LEFT_CLOCK_PIN            p14
-#define MOTOR_LEFT_DIR_PIN              p12
-#define MOTOR_RIGHT_CLOCK_PIN           p11
-#define MOTOR_RIGHT_DIR_PIN             p10
+#define MOTOR_LEFT_CLOCK_PIN            p14         // Left motor clock generate
+#define MOTOR_LEFT_DIR_PIN              p12         // Left motor direction control
+#define MOTOR_RIGHT_CLOCK_PIN           p11         // Right motor clock generate
+#define MOTOR_RIGHT_DIR_PIN             p10         // Right motor direction
 
-#define LEFT_PROXIMITY_PIN              p9
-#define CENTER_PROXIMITY_PIN            p6
-#define RIGHT_PROXIMITY_PIN             p8 
+#define LEFT_PROXIMITY_PIN              p9          // Back panel - left proximity sensor
+#define CENTER_PROXIMITY_PIN            p6          // Front panel - centre proximity sensor
+#define RIGHT_PROXIMITY_PIN             p8          // Back panel - right proximity sensor
 
-#define SET_SWITCH_PIN                  p27
-#define BEGIN_SWITCH_PIN                p28
+#define SET_SWITCH_PIN                  p27         // Set switch
+#define BEGIN_SWITCH_PIN                p28         // Reset switch
 
 // PC1602F 16x2 Text LCD Pins:
-#define LCD_RS_PIN                      p21
-#define LCD_E_PIN                       p22
-#define LCD_D0_PIN                      p23
-#define LCD_D1_PIN                      p24
-#define LCD_D2_PIN                      p25
-#define LCD_D3_PIN                      p26
+#define LCD_RS_PIN                      p21         // Register select
+#define LCD_E_PIN                       p22         // Enable pin
+#define LCD_D0_PIN                      p23         // Data pin 0
+#define LCD_D1_PIN                      p24         // Data pin 1
+#define LCD_D2_PIN                      p25         // Data pin 2
+#define LCD_D3_PIN                      p26         // Data pin e
 
 // HC-SR04 Ultrasonic Sensor Pins:
-#define ULTRAOSNIC_TX_PIN               p29
-#define ULTRASONIC_RX_PIN               p30
+#define ULTRASONIC_TX_PIN               p29         // Transmit
+#define ULTRASONIC_RX_PIN               p30         // Receive
 
 #endif
